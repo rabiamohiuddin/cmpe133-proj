99052e722ecb1300b58a4e7af39db290c306ec60
Rabia Mohiuddin <rabia@mohiuddin.com>
2019-03-01 12:25:47 -0800

    Update fakeProductService.js

--- src/services/fakeProductService.js
+++ src/services/fakeProductService.js
@@ -3,8 +3,8 @@ import * as aislesAPI from "./fakeAisleService";
 const products = [
   {
     _id: "5b21ca3eeb7f6fbccd471815",
-    title: "Cheerios",
-    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Cereal" },
+    name: "Cheerios",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471818", name: "Cereal" },
     numberInStock: 6,
     basePrice: 4.99,
     salePrice: 3.99,
@@ -12,8 +12,8 @@ const products = [
   },
   {
     _id: "5b21ca3eeb7f6fbccd471816",
-    title: "Frosted Flakes",
-    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Cereal" },
+    name: "Frosted Flakes",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471818", name: "Cereal" },
     numberInStock: 5,
     basePrice: 4.99,
     salePrice: 2.5,
@@ -21,8 +21,8 @@ const products = [
   },
   {
     _id: "5b21ca3eeb7f6fbccd471817",
-    title: "O Organics Organic Eggs Large Brown - 12 Count",
-    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Eggs" },
+    name: "O Organics Organic Eggs Large Brown - 12 Count",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471820", name: "Eggs" },
     numberInStock: 8,
     basePrice: 5.69,
     salePrice: 0,
@@ -30,8 +30,8 @@ const products = [
   },
   {
     _id: "5b21ca3eeb7f6fbccd471819",
-    title: "Chips Ahoy!",
-    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Cookies" },
+    name: "Chips Ahoy!",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471814", name: "Cookies" },
     numberInStock: 7,
     basePrice: 4.39,
     salePrice: 2.99,
@@ -39,8 +39,8 @@ const products = [
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181a",
-    title: "Oreos",
-    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Cookies" },
+    name: "Oreos",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471814", name: "Cookies" },
     numberInStock: 7,
     basePrice: 4.99,
     salePrice: 3.33,
@@ -48,8 +48,8 @@ const products = [
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181b",
-    title: "Newtons",
-    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Cookies" },
+    name: "Newtons",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471814", name: "Cookies" },
     numberInStock: 7,
     basePrice: 5.99,
     salePrice: 5.49,
@@ -57,8 +57,8 @@ const products = [
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181e",
-    title: "Egglands Best Eggs Large - 12 Count",
-    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Eggs" },
+    name: "Egglands Best Eggs Large - 12 Count",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471820", name: "Eggs" },
     numberInStock: 7,
     basePrice: 5.49,
     salePrice: 0,
@@ -66,8 +66,8 @@ const products = [
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181f",
-    title: "Value Corner Eggs Large Family Pack - 18 Count",
-    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Eggs" },
+    name: "Value Corner Eggs Large Family Pack - 18 Count",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471820", name: "Eggs" },
     numberInStock: 4,
     basePrice: 4.59,
     salePrice: 0,
@@ -75,8 +75,8 @@ const products = [
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821",
-    title: "Lucky Charms",
-    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Cereal" },
+    name: "Lucky Charms",
+    aisle: { _id: "5b21ca3eeb7f6fbccd471818", name: "Cereal" },
     numberInStock: 7,
     basePrice: 4.99,
     salePrice: 3.50,
