b212a1537eb5c69de9630bc62f8579658e3e3424
Rabia Mohiuddin <rabia@mohiuddin.com>
2019-03-01 12:47:13 -0800

    Update fakeAisleService.js

--- src/services/fakeAisleService.js
+++ src/services/fakeAisleService.js
@@ -3,7 +3,7 @@ export const aisles = [
     { _id: "5b21ca3eeb7f6fbccd471814", name: "Cookies" },
     { _id: "5b21ca3eeb7f6fbccd471820", name: "Eggs" },
     { _id: "5b21ca3eeb7f6fbccd471822", name: "Produce" },
-    { _id: "5b21ca3eeb7f6fbccd471824", name: "Personal Care" },
+    { _id: "5b21ca3eeb7f6fbccd471824", name: "Health" },
     { _id: "5b21ca3eeb7f6fbccd471828", name: "Home" }
   ];
   
