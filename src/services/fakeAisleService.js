3dd6ac5c057c7e2ace726781c2ad8968c86cc8c7
Rabia Mohiuddin <rabia@mohiuddin.com>
2019-03-04 09:52:33 -0800

    Update fakeAisleService.js

--- src/services/fakeAisleService.js
+++ src/services/fakeAisleService.js
@@ -4,7 +4,13 @@ export const aisles = [
     { _id: "5b21ca3eeb7f6fbccd471820", name: "Eggs" },
     { _id: "5b21ca3eeb7f6fbccd471822", name: "Produce" },
     { _id: "5b21ca3eeb7f6fbccd471824", name: "Health" },
-    { _id: "5b21ca3eeb7f6fbccd471828", name: "Home" }
+    { _id: "5b21ca3eeb7f6fbccd471828", name: "Home" },
+    { _id: "5b21ca3eeb7f6fbccd471832", name: "Canned Items" }
+    { _id: "5b21ca3eeb7f6fbccd471834", name: "Snacks and Candy" },
+    { _id: "5b21ca3eeb7f6fbccd471836", name: "Condiments, Spices, and Baking" },
+    { _id: "5b21ca3eeb7f6fbccd471838", name: "Beverages" },
+    { _id: "5b21ca3eeb7f6fbccd471840", name: "Baby Care" },
+    { _id: "5b21ca3eeb7f6fbccd471838", name: "Frozen Foods" }
   ];
   
   export function getAisles() {
