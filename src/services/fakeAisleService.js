faf090b1266700d0404761068d31e31b2d8fba22
Rabia Mohiuddin <rabia@mohiuddin.com>
2019-03-04 10:42:54 -0800

    Update fakeAisleService.js

--- src/services/fakeAisleService.js
+++ src/services/fakeAisleService.js
@@ -5,7 +5,7 @@ export const aisles = [
     { _id: "5b21ca3eeb7f6fbccd471822", name: "Produce" },
     { _id: "5b21ca3eeb7f6fbccd471824", name: "Health" },
     { _id: "5b21ca3eeb7f6fbccd471828", name: "Home" },
-    { _id: "5b21ca3eeb7f6fbccd471832", name: "Canned Items" }
+    { _id: "5b21ca3eeb7f6fbccd471832", name: "Canned Foods" }
     { _id: "5b21ca3eeb7f6fbccd471834", name: "Snacks and Candy" },
     { _id: "5b21ca3eeb7f6fbccd471836", name: "Condiments, Spices, and Baking" },
     { _id: "5b21ca3eeb7f6fbccd471838", name: "Beverages" },
